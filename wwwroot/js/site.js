7645a0fd308f69b34dd4efdecaeed59ab3b7af3f
ユーザー <siromk177@gmail.com>
2024-05-31 13:19:24 +0900

    カレンダーと天気の表示を修正

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -44,28 +44,27 @@ function createProcess(year, month) {
     var lastMonthEndDate = new Date(year, month, 0).getDate();
     var row = Math.ceil((startDayOfWeek + endDate) / week.length);
 
-// Write your JavaScript code.
+    let url = "https://www.jma.go.jp/bosai/forecast/data/forecast/400000.json";
 
-let url = "https://www.jma.go.jp/bosai/forecast/data/forecast/400000.json";
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (weather) {
+            console.log(weather);
+            // 特定の地域(今回は福岡)だけ選択して変数に詰め直す
+            let area = weather[0].timeSeries[0].areas[0];
+            console.log(area);
+            // 発表者と報告日時の情報を画面に書き出す
+            document.getElementById("publishingOffice").lastElementChild.textContent = weather[0].publishingOffice;
+            document.getElementById("reportDatetime").lastElementChild.textContent = weather[0].reportDatetime;
+            // 特定地域の情報を画面に書き出す
+            document.getElementById("targetArea").lastElementChild.textContent = area.area.name;
+            document.getElementById("today").lastElementChild.textContent = area.weathers[0];
+            document.getElementById("tomorrow").lastElementChild.textContent = area.weathers[1];
+            document.getElementById("dayAfterTomorrow").lastElementChild.textContent = area.weathers[2];
+        });
 
-fetch(url)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (weather) {
-        console.log(weather);
-        // 特定の地域(今回は福岡)だけ選択して変数に詰め直す
-        let area = weather[0].timeSeries[0].areas[0];
-        console.log(area);
-        // 発表者と報告日時の情報を画面に書き出す
-        document.getElementById("publishingOffice").lastElementChild.textContent = weather[0].publishingOffice;
-        document.getElementById("reportDatetime").lastElementChild.textContent = weather[0].reportDatetime;
-        // 特定地域の情報を画面に書き出す
-        document.getElementById("targetArea").lastElementChild.textContent = area.area.name;
-        document.getElementById("today").lastElementChild.textContent = area.weathers[0];
-        document.getElementById("tomorrow").lastElementChild.textContent = area.weathers[1];
-        document.getElementById("dayAfterTomorrow").lastElementChild.textContent = area.weathers[2];
-    });
     // 1行ずつ設定
     for (var i = 0; i < row; i++) {
         calendar += "<tr>";
